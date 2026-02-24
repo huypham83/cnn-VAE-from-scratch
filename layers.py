2999f6340d2f1e32315a096b57d442eda25ff8c5
huypham83 <25520726@gm.uit.edu.vn>
2026-02-24 10:36:59 +0700

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,5 +1,5 @@
 import cupy as cp
-from utils import im2col, col2im
+from utils import *
 
 class Activation():
     def __init__(self, func=None):
@@ -7,6 +7,10 @@ class Activation():
             case 'relu':
                 self.forward = self._relu_forward
                 self.backward = self._relu_backward
+            case 'leaky_relu':
+                self.alpha = 0.2
+                self.forward = self._lrelu_forward
+                self.backward = self._lrelu_backward
             case 'sigmoid':
                 self.forward = self._sigmoid_forward
                 self.backward = self._sigmoid_backward
@@ -19,7 +23,14 @@ class Activation():
         return cp.maximum(0, x)
     def _relu_backward(self, out_grad):
         return (self.x > 0).astype(float) * out_grad
-
+    
+    def _lrelu_forward(self, x):
+        self.x = x
+        return cp.where(x > 0, x, self.alpha * x)
+    def _lrelu_backward(self, out_grad):
+        grad_mask = cp.where(self.x > 0, 1, self.alpha)
+        return grad_mask * out_grad
+    
     def _sigmoid_forward(self, x):
         self.output = 1 / (1 + cp.exp(-x))
         return self.output
@@ -68,12 +79,26 @@ class VAELoss():
         kl_grad_mean = mean / batch_size
         kl_grad_var = -0.5 * (1 - cp.exp(log_var)) / batch_size
         return mse_grad, kl_grad_mean, kl_grad_var
+    
+class BCELoss():
+    def __init__(self):
+        self.eps = 1e-7
+    def forward(self, y_pred, y_true):
+        y_pred = cp.clip(y_pred, self.eps, 1 - self.eps)
+
+        self.loss = -cp.mean(y_true * cp.log(y_pred) + (1 - y_true) * cp.log(1 - y_pred))
+        return self.loss
+    def backward(self, y_pred, y_true):
+        batch_size = y_pred.shape[0]
+        y_pred = cp.clip(y_pred, self.eps, 1 - self.eps)
+        
+        grad = -(y_true / y_pred) + ((1 - y_true) / (1 - y_pred))
+        return grad / batch_size
 
 class Dense():
-    def __init__(self, input_size, output_size, activation = None, lr = 0.001):
+    def __init__(self, input_size, output_size, activation = None):
         self.output_size = output_size
         self.input_size = input_size
-        self.lr = lr
         std = 1 / cp.sqrt(input_size)
         self.W = cp.random.uniform(-std, std, (input_size, output_size))
         self.b = cp.zeros(output_size)
@@ -86,23 +111,23 @@ class Dense():
         out_grad = self.act.backward(out_grad)
         self.dz = out_grad
 
-        self.dw = self.input.T @ self.dz
+        self.dW = self.input.T @ self.dz
         self.db = cp.sum(self.dz, axis = 0)
         self.inp_grad = self.dz @ self.W.T
-
-        self.W -= self.lr * self.dw
-        self.b -= self.lr * self.db
+        
         return self.inp_grad
 
+    def get_params(self):
+        return [('W', 'dW'), ('b', 'db')]
+    
 class Conv2D():
-    def __init__(self, in_channel, out_channel, kernel_height, kernel_width, stride=1, activation = None, lr=0.001):
+    def __init__(self, in_channel, out_channel, kernel_height, kernel_width, stride=1, activation = None):
         self.in_channel = in_channel
         self.out_channel = out_channel
         self.kernel_height = kernel_height
         self.kernel_width = kernel_width
         self.stride = stride
         self.padding = max(kernel_height, kernel_width) // 2
-        self.lr = lr
 
         std = 1 / cp.sqrt(in_channel * kernel_width * kernel_height)
         self.W = cp.random.uniform(-std, std, (out_channel, in_channel, kernel_height, kernel_width))
@@ -137,14 +162,14 @@ class Conv2D():
 
         inp_grad = col2im(d_cols, self.x_shape, self.kernel_height, self.kernel_width, self.padding, self.stride)
 
-        self.W -= self.lr * self.dW
-        self.b -= self.lr * self.db
-
         return inp_grad
+    
+    def get_params(self):
+        return [('W', 'dW'), ('b', 'db')]
 
 # use even size kernel only bcz uhhh math
 class TransposeConv2D():
-    def __init__(self, in_channel, out_channel, kernel_height, kernel_width, stride=1, padding=-1, activation = None, lr=0.001):
+    def __init__(self, in_channel, out_channel, kernel_height, kernel_width, stride=1, padding=-1, activation = None):
         self.in_channel = in_channel
         self.out_channel = out_channel
         self.kernel_height = kernel_height
@@ -154,7 +179,6 @@ class TransposeConv2D():
             self.padding = max(kernel_height, kernel_width) // 2
         else:
             self.padding = padding
-        self.lr = lr
 
         std = 1 / cp.sqrt(in_channel * kernel_width * kernel_height)
         self.W = cp.random.uniform(-std, std, (in_channel, out_channel, kernel_height, kernel_width))
@@ -187,6 +211,7 @@ class TransposeConv2D():
         self.inp_grad = self.inp_grad.reshape(self.in_channel, self.H_in, self.W_in, -1).transpose(3, 0, 1, 2)
         self.db = cp.sum(out_grad, axis=(0, 2, 3), keepdims=True).reshape(self.out_channel, -1)
 
-        self.W -= self.lr * self.dW
-        self.b -= self.lr * self.db
         return self.inp_grad
+    
+    def get_params(self):
+        return [('W', 'dW'), ('b', 'db')]
